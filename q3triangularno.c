629c0609a2906b39c0d7bfdbea9feea5862fa057
vidhyalakshmi22121996 <vidhyalakshmi2212@gmail.com>
2017-01-12 17:18:37 +0530

    Create q3triangularno.c

---
+++ q3triangularno.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) 
+{
+    int i,n,count=1,op=1;
+    printf("Enter any no");
+    scanf("%d",&n);
+  	for(i=0;i<n;i++)
+	 {
+        printf("\t%d",op);
+        count++;
+	      op=op+count;
+	 }
+	 return 0;
+}
