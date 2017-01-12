97db916c103ccc643e990c9b1d5054c8fca123e1
vidhyalakshmi22121996 <vidhyalakshmi2212@gmail.com>
2017-01-12 17:20:03 +0530

    Update q3triangularno.c

--- q3triangularno.c
+++ q3triangularno.c
@@ -4,11 +4,11 @@ int main(void)
     int i,n,count=1,op=1;
     printf("Enter any no");
     scanf("%d",&n);
-  	for(i=0;i<n;i++)
-	 {
+    for(i=0;i<n;i++)
+    {
         printf("\t%d",op);
         count++;
-	      op=op+count;
-	 }
-	 return 0;
+	op=op+count;
+    }
+    return 0;
 }
