00041038c974973317c088f75fdd630e8a25e70e
vidhyalakshmi22121996 <vidhyalakshmi2212@gmail.com>
2017-01-16 18:20:00 +0530

    Create nextpalindrome.c

---
+++ nextpalindrome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+	int j,l,n,m;
+	scanf("%d",&n);
+	l=n;
+	j=l/100;
+	while(n!=0)
+	{
+	    m=n;
+        n=n/10;
+	   
+	}
+	m=j;
+	printf("%d0%d",j,m);
+	return 0;
+}
