8e1b8bf2861e774e138964813dfadadedbe101ab
vidhyalakshmi22121996 <vidhyalakshmi2212@gmail.com>
2017-01-26 14:59:04 +0530

    Update q4euler.c

--- q4euler.c
+++ q4euler.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main()
 {
-  int m[10][10]={0},i,j,n;
-  printf("\n Enter any number\n");
-  scanf("%d",&n);
-  for(i=2;i<(n+1);i++)
-  {
-    for(j=1;j<i;j++)
+    int m[10][10]={0},i,j,n;
+    printf("\n Enter any number\n");
+    scanf("%d",&n);
+    for(i=2;i<(n+1);i++)
     {
-      if(j==1||j==i-1)
-      {
-        m[i][j]=1;
-      }
-      else
-      {
-        m[i][j]=((i-j)*m[i-1][j-1])+((j)*m[i-1][j]);
-      }
-      printf("%d\t",m[i][j]);
+        for(j=1;j<i;j++)
+        {
+            if(j==1||j==i-1)
+            {
+                m[i][j]=1;
+            }
+            else
+            {
+                m[i][j]=((i-j)*m[i-1][j-1])+((j)*m[i-1][j]);
+            }
+            printf("%d\t",m[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
-  return 0;
+    return 0;
 }
